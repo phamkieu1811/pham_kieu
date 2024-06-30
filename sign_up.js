cfd74abd5605e2b73e4cdb4a3eab568499c601e6
phamkieu1811 <susuks8862@gmail.com>
2024-06-30 12:30:40 +0700

    Add files via upload

---
+++ sign_up.js
@@ -0,0 +1,93 @@
+
+import './sigup.css';
+
+export default function SigUp() {
+    return (
+
+        <div className="sign-up">
+            <div className="div">
+                <div className="frame">
+                    <div className="frame-2">
+                        <div className="frame-3">
+                            <div className="frame-4">
+                                <div className="text-wrapper">Hi, Get Started Now</div>
+                                <img
+                                    className="emoji-waving-hand"
+                                    alt="Emoji waving hand"
+                                    src="https://c.animaapp.com/jZdAdLot/img/---emoji--waving-hand-@2x.png"
+                                />
+                            </div>
+                            <p className="p">Enter details to create your Travel Pulse account</p>
+                        </div>
+                        <div className="frame-5">
+                            <div className="frame-6">
+                                <div className="frame-7">
+                                    <img
+                                        className="img"
+                                        alt="Logos google icon"
+                                        src="https://c.animaapp.com/jZdAdLot/img/logos-google-icon.svg"
+                                    />
+                                    <div className="text-wrapper-2">Continue with Google</div>
+                                </div>
+                                <div className="group">
+                                    <div className="rectangle" />
+                                    <div className="rectangle-2" />
+                                    <div className="text-wrapper-3">or</div>
+                                </div>
+                            </div>
+                            <div className="frame-wrapper">
+                                <div className="frame-8">
+                                    <div className="div-wrapper">
+                                        <div className="text-wrapper-4">Full Name</div>
+                                    </div>
+                                    <div className="div-wrapper">
+                                        <div className="text-wrapper-4">Email Address</div>
+                                    </div>
+                                    <div className="frame-9">
+                                        <div className="text-wrapper-4">Password</div>
+                                        <img
+                                            className="img"
+                                            alt="Ic baseline remove"
+                                            src="https://c.animaapp.com/jZdAdLot/img/ic-baseline-remove-red-eye-1.svg"
+                                        />
+                                    </div>
+                                    <div className="frame-9">
+                                        <div className="text-wrapper-4">Confirm Password</div>
+                                        <img
+                                            className="img"
+                                            alt="Ic baseline remove"
+                                            src="https://c.animaapp.com/jZdAdLot/img/ic-baseline-remove-red-eye-1.svg"
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="frame-10">
+                        <div className="frame-11">
+                            <div className="text-wrapper-5">Sign Up</div>
+                        </div>
+                        <div className="frame-12">
+                            <div className="text-wrapper-6">Already have an account?</div>
+                            <div className="text-wrapper-7">Sign in to account</div>
+                        </div>
+                    </div>
+                </div>
+                <div className="frame-13">
+                    <div className="overlap-group-wrapper">
+                        <div className="overlap-group">
+                            <div className="rectangle-3" />
+                            <img
+                                className="image-removebg"
+                                alt="Imageremovebg"
+                                src="https://c.animaapp.com/jZdAdLot/img/image-1--5--removebg-preview-2@2x.png"
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    )
+
+}
