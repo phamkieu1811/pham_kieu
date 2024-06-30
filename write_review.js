a13f265693921f0a670fc671dacdb59dbeb908b2
phamkieu1811 <susuks8862@gmail.com>
2024-07-01 04:52:36 +0700

    Add files via upload

---
+++ write_review.js
@@ -0,0 +1,107 @@
+import React from "react";
+import './write_review.css';
+
+export default function WriteReviews () {
+  return (
+    <div className="write-reviews">
+      <div className="div">
+        <div className="frame">
+          <p className="text-wrapper">What would you like to review?</p>
+          <img className="img" alt="Akar icons search" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Search_Icon.svg/1024px-Search_Icon.svg.png" />
+        </div>
+        <div className="frame-wrapper">
+          <div className="frame-2">
+            <img className="image-removebg" alt="Image_removebg" src="https://c.animaapp.com/lLxM5gOC/img/image-1--5--removebg-preview-1-1@2x.png" />
+            <p className="p">© 2023 Travel Pulse. All rights reserved</p>
+          </div>
+        </div>
+        <p className="text-wrapper-2">Share your Travel Experience in form of a story</p>
+        <div className="frame-3">
+          <div className="frame-4">
+            <div className="text-wrapper-3">Top places with reviews</div>
+            <p className="text-wrapper-4">Travelers want to see more reviews of these places.</p>
+          </div>
+          <div className="frame-5">
+            <img className="img" alt="Ic baseline edit" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrQBmN0irSetwVrDf311jMGWt-n9LCIk2YJg&s" />
+            <div className="text-wrapper-5">Write New Review</div>
+          </div>
+        </div>
+        <div className="frame-6">
+          <img className="frame-7" alt="Frame" src="https://png.pngtree.com/png-vector/20190115/ourlarge/pngtree-camera-icon--line-style-vector-illustration-png-image_314753.jpg" />
+          <div className="frame-8">
+            <div className="frame-9">
+              <div className="frame-10">
+                <div className="frame-11">
+                  <div className="text-wrapper-6">Title of your review</div>
+                  <div className="div-wrapper">
+                    <div className="text-wrapper-7">Summarize your Travel Journey</div>
+                  </div>
+                </div>
+                <div className="frame-11">
+                  <div className="text-wrapper-6">Your review</div>
+                  <div className="div-wrapper">
+                    <p className="text-wrapper-8">
+                      A detailed review of your Travel Journey. Travellers will love to know your experience
+                    </p>
+                  </div>
+                </div>
+                <div className="frame-12">
+                  <div className="frame-11">
+                    <div className="text-wrapper-6">Location</div>
+                    <div className="frame-13">
+                      <div className="text-wrapper-7">Enter Travel Location</div>
+                    </div>
+                  </div>
+                  <div className="frame-11">
+                    <div className="text-wrapper-6">When did you travel?</div>
+                    <div className="frame-14">
+                      <div className="text-wrapper-7">Select One</div>
+                      <img
+                        className="material-symbols"
+                        alt="Material symbols"
+                        src="https://i.pinimg.com/564x/0f/87/26/0f8726ce8f5699bf34b6ef09543852da.jpg"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="frame-15">
+                <div className="frame-16" />
+                <p className="text-wrapper-9">
+                  I certify that the information in this review is based solely on my own experiences with the product
+                  or service in question. I also attest that I have no personal or professional affiliation with the
+                  business in question and have not been given any incentives or payment from the business to write this
+                  review. I am aware that fake reviews are strictly prohibited on Tripadvisor.
+                </p>
+              </div>
+            </div>
+            <div className="frame-17">
+              <div className="text-wrapper-10">Submit Review</div>
+            </div>
+          </div>
+        </div>
+        <div className="frame-18">
+          <div className="image-removebg-wrapper">
+          <img className="image-removebg-2" alt="Image_removebg" src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSalFsZBY6yafZOA_0qwSTbjszEOnCHmUb9-ZoB37g3l98fFDTY" />
+          </div>
+          <div className="frame-19">
+            <div className="text-wrapper-11">Destination</div>
+            <div className="text-wrapper-11">Stories</div>
+            <div className="text-wrapper-11">Reviews</div>
+          </div>
+          <div className="frame-20">
+            
+            <div className="frame-21">
+              <div className="text-wrapper-12">Sign Up</div>
+              <div className="frame-22">
+                <div className="text-wrapper-10">Login</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+
