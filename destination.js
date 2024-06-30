97798be3790178102d859fccc6dd8ef4c5979b5a
phamkieu1811 <susuks8862@gmail.com>
2024-06-30 16:36:04 +0700

    Add files via upload

---
+++ destination.js
@@ -0,0 +1,218 @@
+import React from "react";
+import './destination.css';
+
+export default function Destination() {
+    return (
+        <div className="destination">
+            <div className="div">
+                <div className="overlap">
+                    <div className="rectangle" />
+                    <p className="text-wrapper">Discovering the wonders of our planet, one adventure at a time</p>
+                    <div className="frame">
+                        <div className="image-removebg-wrapper">
+                            <img className="image-removebg" alt="Imageremovebg" src="https://c.animaapp.com/lLxM5gOC/img/image-1--5--removebg-preview-1-1@2x.png" />
+                        </div>
+                        <div className="frame-2">
+                            <div className="text-wrapper-2">Destination</div>
+                            <div className="text-wrapper-2">Stories</div>
+                            <div className="text-wrapper-2">Reviews</div>
+                        </div>
+                        <div className="frame-3">
+                            <img className="img" alt="Akar icons search" src="https://c.animaapp.com/lLxM5gOC/img/akar-icons-search-1.svg" />
+                            <div className="frame-4">
+                                <div className="text-wrapper-3">Sign Up</div>
+                                <div className="div-wrapper">
+                                    <div className="text-wrapper-4">Login</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="frame-5">
+                    <div className="frame-6">
+                        <p className="p">Search for places, hotels or restaurants</p>
+                        <img className="img-2" alt="Akar icons search" src="https://c.animaapp.com/lLxM5gOC/img/akar-icons-search-2.svg" />
+                    </div>
+                    <div className="frame-7">
+                        <div className="frame-8">
+                            <p className="text-wrapper-5">Filter by type of place</p>
+                            <div className="frame-9">
+                                <div className="text-wrapper-6">All Types</div>
+                                <img className="img" alt="Material symbols" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFIOBuStOPP6Uo6AZH2r_ZL80khNQkJSiTivy1c9NCdSNVLUWBw31qKVnmh5luhbV9crM&usqp=CAU" />
+                            </div>
+                        </div>
+                        <div className="frame-8">
+                            <div className="text-wrapper-5">Sort by</div>
+                            <div className="frame-9">
+                                <div className="text-wrapper-6">Most Popular</div>
+                                <img className="img" alt="Material symbols" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFIOBuStOPP6Uo6AZH2r_ZL80khNQkJSiTivy1c9NCdSNVLUWBw31qKVnmh5luhbV9crM&usqp=CAU" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="frame-10">
+                    <div className="frame-11">
+                        <div className="frame-12">
+                            <div className="image-wrapper">
+                                <img className="image" alt="Image10" src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSjAL7L4LKgjHFyeoaCJzbBKZ2whJtBCl88RFDqdbvKjbSB5hEb" />
+                            </div>
+                            <div className="frame-13">
+                                <div className="text-wrapper-7">Asia</div>
+                                <div className="text-wrapper-8">China</div>
+                            </div>
+                        </div>
+                        <div className="frame-12">
+                            <div className="img-wrapper">
+                                <img className="image" alt="Image11" src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcT0dzkdIoXKBUmhZcKFop9JLxpY6Ae4_dgirUE46ajCIRw4oOMa" />
+                            </div>
+                            <div className="frame-13">
+                                <div className="text-wrapper-7">Africa</div>
+                                <div className="text-wrapper-8">Namibia</div>
+                            </div>
+                        </div>
+                        <div className="frame-12">
+                            <div className="overlap-group-wrapper">
+                                <div className="overlap-group">
+                                    <img className="image" alt="Image12" src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSxH1HsxObg5DpuZqbQf5cUgnPY96E_afDUJRTikh_ZdANAYizC" />
+                                </div>
+                            </div>
+                            <div className="frame-13">
+                                <div className="text-wrapper-7">Australia</div>
+                                <div className="text-wrapper-8">Victoria</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="frame-11">
+                        <div className="frame-12">
+                            <div className="frame-14">
+                                <img className="image" alt="Image." src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRCrJhoZe1Ll_fyewEthhTgOma_EAFaiCoQldK_63XUdLqvHn3U" />
+                            </div>
+                            <div className="frame-13">
+                                <div className="text-wrapper-7">Europe</div>
+                                <div className="text-wrapper-8">Denmark</div>
+                            </div>
+                        </div>
+                        <div className="frame-12">
+                            <div className="frame-15">
+                                <img className="image" alt="Image11-2" src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTUi4tHOeB63c2GlXaTVmlJHAZjGPUvQac8aieZXAJoaykb4dTY" />
+                            </div>
+                            <div className="frame-13">
+                                <div className="text-wrapper-7">Caribbean</div>
+                                <div className="text-wrapper-8">Dominican Republic</div>
+                            </div>
+                        </div>
+                        <div className="frame-12">
+                            <div className="overlap-group-wrapper">
+                                <div className="overlap-group-2">
+                                    <img className="image" alt="Image12-2" src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTjwZk9diEa9QwzWyP2JEWv-AVx7JWw2MM9yTYPHzsv_LNzgvcE" />
+                                </div>
+                            </div>
+                            <div className="frame-13">
+                                <div className="text-wrapper-7">Canada</div>
+                                <div className="text-wrapper-8">Ontario</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="frame-11">
+                        <div className="frame-12">
+                            <div className="frame-16">
+                                <img className="image" alt="Image10-2" src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTSoHkhuLEpWKZKFcoeu00eoqdmGDNIUWYatiZdvNlBYoSyvpKj" />
+                            </div>
+                            <div className="frame-13">
+                                <div className="text-wrapper-7">Africa</div>
+                                <div className="text-wrapper-8">Nigeria</div>
+                            </div>
+                        </div>
+                        <div className="frame-12">
+                            <div className="frame-17">
+                                <img className="image" alt="Image11-3" src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRdquMNfj_hIROre8nNrxb5yZSGXOrLbwam0ELZ8eCoMhrzmhPW" />
+                            </div>
+                            <div className="frame-13">
+                                <div className="text-wrapper-7">Europe</div>
+                                <div className="text-wrapper-8">Greece</div>
+                            </div>
+                        </div>
+                        <div className="frame-12">
+                            <div className="overlap-group-wrapper">
+                                <div className="overlap-group-3">
+                                    <img className="image" alt="Image12-3" src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRlh1nNHL_387tO18OQS3ZOceYmxa22RZ3jIZe-KCIssCD_Ejcu" />
+                                    <img className="image" alt="Image15" src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTZmhfwAh0EnJz_MkH8hN_uNOfxzIJ-KZhFK873H85ZEp9SZmoj" />
+                                </div>
+                            </div>
+                            <div className="frame-13">
+                                <div className="text-wrapper-7">Asia</div>
+                                <div className="text-wrapper-8">Japan</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="frame-11">
+                        <div className="frame-12">
+                            <div className="frame-18">
+                                <img className="image" alt="Image10-3" src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSV6dLrTM-GHpdzfwrZLbpo33W9fjb5ARgPCQmsvNvMgSjTVgRk" />
+                            </div>
+                            <div className="frame-13">
+                                <div className="text-wrapper-7">South America</div>
+                                <div className="text-wrapper-8">Brazil</div>
+                            </div>
+                        </div>
+                        <div className="frame-12">
+                            <div className="frame-19">
+                                <img className="image" alt="Image11-4" src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTw5tUaTigWfYZr-Z91XBs6xtUfvNXJBff9N0saNwXD5P-NUTRZ" />
+                            </div>
+                            <div className="frame-13">
+                                <div className="text-wrapper-7">North America</div>
+                                <div className="text-wrapper-8">Hawaii</div>
+                            </div>
+                        </div>
+                        <div className="frame-12">
+                            <div className="overlap-group-wrapper">
+                                <div className="overlap-group-4">
+                                    <img className="image" alt="Image12-4" src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQ8dyo9Bdl6s7xIUvn31aHbXAkweZgmPA1bo9YW8mjYmhf5nj4z" />
+                                    <img className="image" alt="Image16" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-hbNDzqrjutM65LriEvp57GrIDGC7hPxfc5ii_jrHvPjg1jz_" />
+                                </div>
+                            </div>
+                            <div className="frame-13">
+                                <div className="text-wrapper-7">Middle East</div>
+                                <div className="text-wrapper-8">Jordan</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="frame-20">
+                    <img className="img-2" alt="Material symbols" src="https://c.animaapp.com/lLxM5gOC/img/vector-14.svg" />
+                    <div className="frame-21">
+                        <div className="frame-22">
+                            <div className="text-wrapper-9">1</div>
+                        </div>
+                        <div className="frame-23">
+                            <div className="text-wrapper-10">2</div>
+                        </div>
+                        <div className="frame-24">
+                            <div className="text-wrapper-10">3</div>
+                        </div>
+                        <div className="frame-24">
+                            <div className="text-wrapper-10">4</div>
+                        </div>
+                        <div className="group-wrapper">
+                            <div className="group">
+                                <div className="ellipse" />
+                                <div className="ellipse-2" />
+                                <div className="ellipse-3" />
+                            </div>
+                        </div>
+                        <div className="frame-24">
+                            <div className="text-wrapper-11">50</div>
+                        </div>
+                    </div>
+                    <img className="img-2" alt="Material symbols" src="https://c.animaapp.com/lLxM5gOC/img/vector-15.svg" />
+                </div>
+                <div className="frame-wrapper">
+                    <div className="frame-25">
+                        <img className="image-removebg-2" alt="Imageremovebg" src="https://c.animaapp.com/lLxM5gOC/img/image-1--5--removebg-preview-1-1@2x.png" />
+                        <p className="text-wrapper-12">© 2023 Travel Pulse. All rights reserved</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
