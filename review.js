67097e546be21e141053fecc666bd03053c9509a
phamkieu1811 <susuks8862@gmail.com>
2024-07-01 07:52:57 +0700

    Add files via upload

---
+++ review.js
@@ -0,0 +1,180 @@
+import React from "react";
+import "./review.css";
+
+export const Review = () => {
+    return (
+        <div className="review">
+            <div className="div-2">
+                <div className="frame-2">
+                    <div className="image-removebg-wrapper">
+                        <img className="image-removebg" alt="Image_removebg" src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSalFsZBY6yafZOA_0qwSTbjszEOnCHmUb9-ZoB37g3l98fFDTY" />
+                    </div>
+                    <div className="frame-3">
+                        <div className="text-wrapper-2">Destination</div>
+                        <div className="text-wrapper-2">Stories</div>
+                        <div className="text-wrapper-2">Reviews</div>
+                    </div>
+                    <div className="frame-4">
+                        <img className="akar-icons-search" alt="Akar icons search" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Search_Icon.svg/1024px-Search_Icon.svg.png" />
+                        <div className="frame-5">
+                            <div className="text-wrapper-3">Sign Up</div>
+                            <div className="div-wrapper">
+                                <div className="text-wrapper-4">Login</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="frame-6">
+                    <p className="p">What would you like to review?</p>
+                    <img className="img" alt="Akar icons search" src="https://e7.pngegg.com/pngimages/940/546/png-clipart-computer-icons-document-business-organization-health-care-magnifier-child-company.png" />
+                </div>
+                <div className="frame-wrapper">
+                    <div className="frame-7">
+                        <img className="image-removebg-2" alt="Image_removebg" src="https://c.animaapp.com/lLxM5gOC/img/image-1--5--removebg-preview-1-1@2x.png" />
+                        <p className="text-wrapper-5">© 2023 Travel Pulse. All rights reserved</p>
+                    </div>
+                </div>
+                <p className="text-wrapper-6">Share your Travel Experience in form of a story</p>
+                <div className="frame-8">
+                    <div className="frame-9">
+                        <div className="text-wrapper-7">Top places with reviews</div>
+                        <p className="text-wrapper-8">Travelers want to see more reviews of these places.</p>
+                    </div>
+                    <div className="frame-10">
+                        <img className="img" alt="Ic baseline edit" src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcS-s9mBm8JZbPVKw7bMjk_DzU27fS4BZj8qBcrMMPSGl3yRu1V2" />
+                        <div className="text-wrapper-9">Write New Review</div>
+                    </div>
+                </div>
+                <div className="frame-11">
+                    <img className="image" alt="Image11" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyn51sS6G3zDG0HtgaESnI1NFVZiImjW2VsvVfDw1-hD6GUM5G" />
+                    <div className="frame-12">
+                        <div className="frame-13">
+                            <div className="frame-9">
+                                <div className="frame-14">
+                                    <div className="text-wrapper-10">Mumbai, India</div>
+                                </div>
+                                <div className="frame-15">
+                                    <div className="frame-16">
+                                        <p className="text-wrapper-11">A Wonderful Journey to India</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="frame-17">
+                                <p className="text-wrapper-12">
+                                    India is a diverse and fascinating country with a rich history, vibrant culture, and stunning
+                                    landscapes.
+                                    <br />
+                                    <br />
+                                    From the bustling cities like Delhi, Mumbai, and Kolkata, to the serene backwaters of Kerala, India
+                                    offers a unique and memorable travel experience for all kinds of travelers. The country is also home
+                                    to many UNESCO World Heritage Sites, such as the Taj Mahal, the ancient city of Hampi, and the Ajanta
+                                    and Ellora Caves.
+                                </p>
+                            </div>
+                        </div>
+                        <div className="frame-1576">
+                            <img className="image-3" alt='' src="https://c.animaapp.com/lLxM5gOC/img/star-1-4.svg" />
+                            <img className="image-4" alt='' src="https://c.animaapp.com/lLxM5gOC/img/star-1-4.svg" />
+                            <img className="image-5" alt='' src="https://c.animaapp.com/lLxM5gOC/img/star-1-4.svg" />
+                            <img className="image-6" alt='' src="https://c.animaapp.com/lLxM5gOC/img/star-1-4.svg" />
+                            <img className="image-7" alt='' src="https://c.animaapp.com/lLxM5gOC/img/star-1-4.svg" />
+                            {/* nút click */}
+
+                        </div>
+                    </div>
+                </div>
+                <div className="frame-18">
+                    <div className="image-wrapper">
+                        <img className="image-2" alt="Image10" src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRGyCi4yX_2YDLvI8anwB03gKPedp40ZumjbHG11xw0yQ1HIYJ-" />
+                    </div>
+                    <div className="frame-12">
+                        <div className="frame-13">
+                            <div className="frame-9">
+                                <div className="frame-14">
+                                    <div className="text-wrapper-13">Barcelona, Spain</div>
+                                </div>
+                                <div className="frame-15">
+                                    <div className="frame-16">
+                                        <p className="text-wrapper-11">A Solo Trip Across Europe</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="frame-17">
+                                <p className="text-wrapper-12">
+                                    Europe is a home to stunning natural landscapes like the Swiss Alps, the Norwegian fjords, and the
+                                    beaches of the Mediterranean coast. The European countries are well-connected through a vast network
+                                    of trains, making it easy to travel across the continent.
+                                    <br />
+                                    <br />
+                                    The art, architecture, and cuisine in each of these cities reflect their unique cultural heritage.,
+                                    are popular tourist attractions, offering visitors a glimpse of the rich history and artistry of these
+                                    cities.
+                                </p>
+                            </div>
+                        </div>
+                        <div className="frame-1576">
+                            <img className="image-3" alt='' src="https://c.animaapp.com/lLxM5gOC/img/star-1-4.svg" />
+                            <img className="image-4" alt='' src="https://c.animaapp.com/lLxM5gOC/img/star-1-4.svg" />
+                            <img className="image-5" alt='' src="https://c.animaapp.com/lLxM5gOC/img/star-1-4.svg" />
+                            <img className="image-6" alt='' src="https://c.animaapp.com/lLxM5gOC/img/star-1-4.svg" />
+                            <img className="image-7" alt='' src="https://c.animaapp.com/lLxM5gOC/img/star-1-4.svg" />
+                            {/* nút click */}
+
+                        </div>
+                    </div>
+                </div>
+                <div className="overlap-group">
+                    <div className="frame-19">
+                        <div className="img-wrapper">
+                            <img className="image-2" alt="Image10" src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQepICHsSweegj1ZaudqNnQNI4sV0MslhbCZlVOTqHmZQ0ZcDhO" />
+                        </div>
+                        <div className="frame-12">
+                            <div className="frame-13">
+                                <div className="frame-9">
+                                    <div className="frame-14">
+                                        <div className="text-wrapper-14">Kathmandu, Nepal</div>
+                                    </div>
+                                    <div className="frame-15">
+                                        <div className="frame-16">
+                                            <p className="text-wrapper-11">11 Best Places to visit in Nepal</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="frame-17">
+                                    <p className="text-wrapper-12">
+                                        I recently spent two weeks in Nepal and it was one of the most memorable trips of my life. The
+                                        people were incredibly friendly and welcoming, and I was amazed by the beauty of the natural
+                                        landscape. The highlight of my trip was trekking in the Himalayas. The views were simply
+                                        breathtaking and the experience of reaching the Everest Base Camp was truly unforgettable.
+                                        <br />
+
+                                        <br />I also spent a day in Chitwan National Park.
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="frame-1576">
+                                <img className="image-3" alt='' src="https://c.animaapp.com/lLxM5gOC/img/star-1-4.svg" />
+                                <img className="image-4" alt='' src="https://c.animaapp.com/lLxM5gOC/img/star-1-4.svg" />
+                                <img className="image-5" alt='' src="https://c.animaapp.com/lLxM5gOC/img/star-1-4.svg" />
+                                <img className="image-6" alt='' src="https://c.animaapp.com/lLxM5gOC/img/star-1-4.svg" />
+                                <img className="image-7" alt='' src="https://c.animaapp.com/lLxM5gOC/img/star-1-4.svg" />
+                                {/* nút click */}
+
+                            </div>
+
+                        </div>
+                    </div>
+                    <div className="frame-19">
+                        <div className="frame-20">
+                            <img className="image-2" alt="Image10-2" src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQepICHsSweegj1ZaudqNnQNI4sV0MslhbCZlVOTqHmZQ0ZcDhO" />
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+
+
